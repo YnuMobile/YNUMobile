0e0f54fc8f7e1edaf088e361b3d1ea7b6ffeb13b
江树金 <532424442@qq.com>
2016-05-17 11:48:00 +0800

    Updata!

--- app/src/main/java/com/imooc/YnuMobile/Fragment/User.java
+++ app/src/main/java/com/imooc/YnuMobile/Fragment/User.java
@@ -1,21 +1,41 @@
 package com.imooc.YnuMobile.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.imooc.YnuMobile.Login.UserLogin;
 import com.imooc.YnuMobile.R;
 
 /**
  * Created by 江树金 on 2016/5/4.
  */
 public class User extends Fragment {
+
+    private Button login;
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
             savedInstanceState) {
-        View view=inflater.inflate(R.layout.user,container,false);
+        view=inflater.inflate(R.layout.user,container,false);
+        initView();
         return view;
     }
+
+    private void initView() {
+        login= (Button) view.findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), UserLogin.class));
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/imooc/YnuMobile/Login/UserLogin.java
@@ -0,0 +1,15 @@
+package com.imooc.YnuMobile.Login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.imooc.YnuMobile.R;
+
+public class UserLogin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
